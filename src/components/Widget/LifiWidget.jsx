af7e6383d956501ad6fe55afd5b71548aac715b6
farman13 <farman327440@gmail.com>
2025-12-24 01:04:32 +0530

    added widget

---
+++ src/components/Widget/LifiWidget.jsx
@@ -0,0 +1,38 @@
+import { useMemo } from "react";
+import { LiFiWidget } from "@lifi/widget";
+
+const basicFeeConfig = {
+    name: "Bigrock-Exchange",
+    logoURI: "https://yourdapp.com/logo.png",
+    fee: 0.005, // 0.5% fee
+    showFeePercentage: true,
+    showFeeTooltip: true
+};
+
+export default function LiFiWidgetComponent() {
+
+    const widgetConfig = useMemo(() => ({
+        variant: "wide",
+        appearance: "dark",
+
+        theme: { container: { border: "1px solid #EAEAEA", borderRadius: "16px", padding: "10px", }, },
+
+        fromChain: 56,
+        toChain: 56,
+
+        fromToken: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+        toToken: "0x55d398326f99059fF775485246999027B3197955",
+
+        fromAmount: "0.01",
+        feeConfig: basicFeeConfig,
+    }), []);
+
+    return (
+        <>
+            <LiFiWidget
+                integrator="Bigrock-Exchange"
+                config={widgetConfig}
+            />
+        </>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,20 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-import { Wallet } from "lucide-react";
 import WalletConfig from "./wallet/WalletConfig.jsx";
 import { Web3Provider } from "../context/Web3Context.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <WalletConfig>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Web3Provider>
-            <App />
-          </Web3Provider>
-        </BrowserRouter>
-      </Provider>
-    </WalletConfig>
-  </StrictMode>
+  <WalletConfig>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Web3Provider>
+          <App />
+        </Web3Provider>
+      </BrowserRouter>
+    </Provider>
+  </WalletConfig>
 );
