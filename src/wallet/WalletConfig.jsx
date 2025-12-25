52f6c991334363f1fae5415bb48138573aef91ee
Tarun Tiwari <tiwaritarun700@gmail.com>
2025-12-26 00:00:21 +0530

    added project id env variable

--- src/wallet/WalletConfig.jsx
+++ src/wallet/WalletConfig.jsx
@@ -19,20 +19,10 @@ import {
 
 const config = getDefaultConfig({
     appName: 'BigRock Exchange',
-    projectId: '9f028ef985d9cf1bacdfea0f961c9a85', // WalletConnect Project ID
+    projectId: process.env.VITE_WALLET_CONNECT_PROJECT_ID, // WalletConnect Project ID
     chains: [mainnet, polygon, optimism, arbitrum, base, sepolia, bsc],
     ssr: false,
-/*************  ✨ Windsurf Command ⭐  *************/
-/**
- * A higher-order component that wraps the WagmiProvider and RainbowKitProvider
- * around the QueryClientProvider, providing a centralized way to manage
- * wallet-related state and queries.
- * @param {Object} props - The props object.
- * @param {ReactNode} props.children - The children to be rendered.
- * @returns {ReactNode} The rendered component.
- */
-/*******  933778f1-0bab-455d-91bf-3a51ee36d4b9  *******/})
-
+})
 const queryClient = new QueryClient()
 
 const WalletInner = ({ children }) => {
